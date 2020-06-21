106415cbecefea46c9fa6cbbf0c1a53c314c1f36
aldiariq <aldi48767@gmail.com>
2020-06-21 13:18:14 +0700

    dashboard pengguna selesai

--- app/src/main/java/com/example/tukarsampah/Dashboard/Kurir/ui/transaksikurir/transaksikurirFragment.java
+++ app/src/main/java/com/example/tukarsampah/Dashboard/Kurir/ui/transaksikurir/transaksikurirFragment.java
@@ -15,7 +15,7 @@ public class transaksikurirFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        View root = inflater.inflate(R.layout.transaksi_kurir_fragment, container, false);
         return root;
     }
 }
