8b077d5ef95d9814555d8f702d97e4863bc0e2cc
aldiariq <aldi48767@gmail.com>
2020-06-27 16:25:03 +0700

    perbaikan beberapa bug

--- app/src/main/java/com/example/tukarsampah/Api/Service.java
+++ app/src/main/java/com/example/tukarsampah/Api/Service.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Service {
-    static final String BASE_URL = "http://192.168.100.20/tukarsampah/";
+    static final String BASE_URL = "http://tukarsampah.freevar.com/tukarsampah/";
     private static Retrofit retrofit;
 
     public static Retrofit Koneksi(){
